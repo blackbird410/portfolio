577259472068470d64c5c6ffdec06ba6f582df1b
Neil Taison Rigaud <neil@Alus.local>
2024-12-24 04:43:23 +0800

    chore: convert tailwind config file export to ES module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
+export default {
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#FF6363',
-                secondary: {
-                    100: '#E2E2D5',
-                    200: '#888883'
-                },
-            },
-            fontFamily: {
-                body: ['Nunito'],
-            },
-            keyframes: {
-                appear: {
-                    '0%': { opacity: '0.25'},
-                    '100%': {opacity: '1'},
-                } 
-            },
-            animation: {
-                appear: 'appear 1s ease-in-out',
-            }
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
         },
-        screens: {
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px'
-        }
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+      keyframes: {
+        appear: {
+          '0%': { opacity: '0.25' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        appear: 'appear 1s ease-in-out',
+      },
     },
-    plugins: [],
-}
-
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+  plugins: [],
+};
